d98e2be86936d5e3cdd2e5113ea29394de0ef93a
guncha25 <guntis.jakovins@gmail.com>
2016-05-11 15:55:28 +0300

    Added StatisticsItemInterface and empty StatisticsItem class.

---
+++ src/StatisticsItemInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\sapi;
+
+use \Drupal\sapi\StatisticsItemInterface;
+
+/**
+ * Interface StatisticsItemInterface.
+ *
+ * @package Drupal\sapi
+ */
+interface StatisticsItemInterface{
+
+}
