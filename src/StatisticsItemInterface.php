c8c0aae94bba4d501293eb37bd2e1eabbadf88b0
guncha25 <guntis.jakovins@gmail.com>
2016-05-12 11:58:50 +0300

    Removed unnecessary use of namespace in StatisticsItem.

--- src/StatisticsItemInterface.php
+++ src/StatisticsItemInterface.php
@@ -8,20 +8,6 @@ namespace Drupal\sapi;
  * @package Drupal\sapi
  */
 interface StatisticsItemInterface {
-    /**
-     * Arbitrary action on a URI.
-     *
-     * @var string
-     */
-    protected $action;
-
-    /**
-     * Route name or other identification that can point to a content item.
-     *
-     * @var string
-     */
-    protected $uri;
-
 
     /**
      * {@inheritdoc}
