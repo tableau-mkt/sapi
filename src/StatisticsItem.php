dcf9e4c273a7bb3ba86f5f3e1ebad5c46d718017
guncha25 <guntis.jakovins@gmail.com>
2016-05-12 11:48:09 +0300

    StatisticsItem constructor,getAction,getUri methods.

--- src/StatisticsItem.php
+++ src/StatisticsItem.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\sapi;
 
+use Drupal\sapi\StatisticsItemInterface;
+
 /**
  * Class StatisticsItem.
  *
@@ -9,4 +11,47 @@ namespace Drupal\sapi;
  */
 class StatisticsItem implements StatisticsItemInterface {
 
+    /**
+     * Arbitrary action on a URI.
+     *
+     * @var string
+     */
+    protected $action;
+
+    /**
+     * Route name or other identification that can point to a content item.
+     *
+     * @var string
+     */
+    protected $uri;
+
+    /**
+     * Statistics item constructor.
+     *
+     * @param array $data
+     *   An associative array containing the event data.
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(array $data) {
+        if (empty($data['action']) || empty($data['uri'])) {
+            throw new \InvalidArgumentException('Action and/or uri can not be empty.');
+        }
+        $this->action = $data['action'];
+        $this->uri = $data['uri'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAction() {
+        return $this->action;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUri() {
+        return $this->uri;
+    }
 }
--- src/StatisticsItemInterface.php
+++ src/StatisticsItemInterface.php
@@ -2,13 +2,34 @@
 
 namespace Drupal\sapi;
 
-use Drupal\sapi\StatisticsItemInterface;
-
 /**
  * Interface StatisticsItemInterface.
  *
  * @package Drupal\sapi
  */
 interface StatisticsItemInterface {
+    /**
+     * Arbitrary action on a URI.
+     *
+     * @var string
+     */
+    protected $action;
 
+    /**
+     * Route name or other identification that can point to a content item.
+     *
+     * @var string
+     */
+    protected $uri;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAction();
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUri();
 }
