5bde71b4fbc057f877d1d29ca13728ab551e1a00
Lauris Igaunis <lauris.igaunis@wunderkraut.com>
2016-05-12 14:01:59 +0300

    Created empty Dummy plugin

---
+++ src/Plugin/StatisticsPlugin/Dummy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\sapi\Plugin\StatisticsPlugin;
+
+use Drupal\sapi\Plugin\StatisticsPluginInterface;
+
+/**
+ * @StatisticsPlugin(
+ *  id = "dummy",
+ *  label = "Dummy label"
+ * )
+ */
+class Dummmy implements StatisticsPluginInterface {
+
+}
