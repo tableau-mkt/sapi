1a6bf6fde554ee36bfe482200ce3173d1f3f84a0
guncha25 <guntis.jakovins@gmail.com>
2016-05-13 11:47:29 +0300

    Injects statistics plugin manager into dispatcher service

--- src/SapiDispatcher.php
+++ src/SapiDispatcher.php
@@ -33,25 +33,41 @@ class SapiDispatcher implements SapiDispatcherInterface {
    * @var \Drupal\Core\Routing\CurrentRouteMatch
    */
   protected $currentRouteMatch;
-  
+
+  /**
+   * Drupal\Component\Plugin\PluginManagerInterface definition.
+   *
+   * @var \Drupal\Component\Plugin\PluginManagerInterface
+   */
+  protected $statisticsPluginManager;
+
   /**
    *
    * @param \Drupal\Core\Config\ConfigManager $configManager
    * @param \Drupal\Core\Session\AccountProxy $currentUser
    * @param \Drupal\Core\Routing\CurrentRouteMatch $currentRouteMatch
+   * @param \Drupal\Component\Plugin\PluginManagerInterface $statisticsPluginManager
    */
-  public function __construct(ConfigManager $configManager, AccountProxy $currentUser, CurrentRouteMatch $currentRouteMatch) {
+  public function __construct(ConfigManager $configManager, AccountProxy $currentUser, CurrentRouteMatch $currentRouteMatch, PluginManagerInterface $statisticsPluginManager) {
     $this->configManager = $configManager;
     $this->currentUser = $currentUser;
     $this->currentRouteMatch = $currentRouteMatch;
+    $this->statisticsPluginManager = \Drupal::service('plugin.manager.statisticsplugin');
   }
 
    /**
     * Dispatches the statistics item to interested parties.
-    * 
+    *
     * @param StatisticsItemInterface $item
+    *
+    * @return void
    */
   public function dispatch(StatisticsItemInterface $item){
+    /** @var \Drupal\sapi\Plugin\StatisticsPluginInterface $instance */
+    foreach ($this->statisticsPluginManager->getDefinitions() as $pluginDefinition) {
+      $instance = $this->statisticsPluginManager->createInstance($pluginDefinition['id']);
+      $instance->process($item);
+    }
 
   }
 
