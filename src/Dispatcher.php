950a3f6303394fb3ebaec8990c0d2b801e0b69c8
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-05-24 13:41:14 +0300

    cleaned out unused dispatcher injections

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -2,9 +2,6 @@
 
 namespace Drupal\sapi;
 
-use Drupal\Core\Config\ConfigManager;
-use Drupal\Core\Session\AccountProxy;
-use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\Component\Plugin\PluginManagerInterface;
 
 /**
@@ -13,27 +10,6 @@ use Drupal\Component\Plugin\PluginManagerInterface;
  * @package Drupal\sapi
  */
 class Dispatcher implements DispatcherInterface {
-
-  /**
-   * Drupal\Core\Config\ConfigManager definition.
-   *
-   * @var \Drupal\Core\Config\ConfigManager
-   */
-  protected $configManager;
-
-  /**
-   * Drupal\Core\Session\AccountProxy definition.
-   *
-   * @var \Drupal\Core\Session\AccountProxy
-   */
-  protected $currentUser;
-
-  /**
-   * Drupal\Core\Routing\CurrentRouteMatch definition.
-   *
-   * @var \Drupal\Core\Routing\CurrentRouteMatch
-   */
-  protected $currentRouteMatch;
 
   /**
    * Drupal\Component\Plugin\PluginManagerInterface definition.
@@ -45,15 +21,9 @@ class Dispatcher implements DispatcherInterface {
   /**
    * Dispatcher constructor.
    *
-   * @param \Drupal\Core\Config\ConfigManager $configManager
-   * @param \Drupal\Core\Session\AccountProxy $currentUser
-   * @param \Drupal\Core\Routing\CurrentRouteMatch $currentRouteMatch
    * @param \Drupal\Component\Plugin\PluginManagerInterface $SAPIActionHandlerPluginManager
    */
-  public function __construct(ConfigManager $configManager, AccountProxy $currentUser, CurrentRouteMatch $currentRouteMatch, PluginManagerInterface $SAPIActionHandlerPluginManager) {
-    $this->configManager = $configManager;
-    $this->currentUser = $currentUser;
-    $this->currentRouteMatch = $currentRouteMatch;
+  public function __construct(PluginManagerInterface $SAPIActionHandlerPluginManager) {
     $this->SAPIActionHandlerPluginManager = $SAPIActionHandlerPluginManager;
   }
 
